25e74dca67c1edcb5f2bb2171c6ec4ed74822313
infy11 <varnitgoyal95@gmail.com>
2022-02-27 19:26:38 +0530

    Changed save to promise

--- controllers/students.js
+++ controllers/students.js
@@ -25,17 +25,14 @@ const postStudent = async (req, res )=> {
     const {roll_no} = req.body;
     req.body.course = course;
     req.body.image = filename;
-    new Students(req.body).save((err,resp)=>{
-        if(err) {
-            console.log('mongoose error', err)
-            res.send({error:true})
-        }
-    }).then(_=>{
+    new Students(req.body).save().then(_=>{
         return fetch(`https://www.fast2sms.com/dev/bulkV2?authorization=LWv32EPO8G4omRDHC1ejYBVAIacidxyb6MQgnsq5hp9TrNJ0SFAFmR9gV1vQPKCNsetT6uY4c0kSWohM&route=v3&sender_id=Cghpet&message=You%20are%20successfully%20registered%20to%20Future%20IT%20academy%20your%20roll%20no%20is%20${roll_no}%20%7B%7D%20please%20contact%20branch%20for%20further%20details%20&language=english&flash=0&numbers=${mob_no}`)
     }).then(res=>{
         return res.json();
     }).then(res=>{
-        console.log('sms response', res);
+        console.log('sms response', res)
+    }).catch(err=>{
+        console.log('error saving student', err);
     })
     res.send({success: true});
 }
