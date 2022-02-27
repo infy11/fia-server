64b14587f066af4b04780f85f904dc5fdf1fd8a1
infy11 <varnitgoyal95@gmail.com>
2022-02-27 18:49:47 +0530

    update app with message support

--- controllers/students.js
+++ controllers/students.js
@@ -1,5 +1,6 @@
 const Students = require('../models/students');
 const Courses = require('../models/courses');
+const fetch = require('node-fetch');
 
 const getStudent = async (req,res) =>{
     const {roll_no} = req.query;
@@ -20,7 +21,8 @@ const postStudent = async (req, res )=> {
     const course = await Courses.CourseModal.findOne({
         courseName: courseName
     }).lean();
-    console.log('found the course', course);
+    const {mob_no} = req.body;
+    const {roll_no} = req.body;
     req.body.course = course;
     req.body.image = filename;
     new Students(req.body).save((err,resp)=>{
@@ -28,6 +30,12 @@ const postStudent = async (req, res )=> {
             console.log('mongoose error', err)
             res.send({error:true})
         }
+    }).then(_=>{
+        return fetch(`https://www.fast2sms.com/dev/bulkV2?authorization=LWv32EPO8G4omRDHC1ejYBVAIacidxyb6MQgnsq5hp9TrNJ0SFAFmR9gV1vQPKCNsetT6uY4c0kSWohM&route=v3&sender_id=Cghpet&message=You%20are%20successfully%20registered%20to%20Future%20IT%20academy%20your%20roll%20no%20is%20${roll_no}%20%7B%7D%20please%20contact%20branch%20for%20further%20details%20&language=english&flash=0&numbers=${mob_no}`)
+    }).then(res=>{
+        return res.json();
+    }).then(res=>{
+        console.log('sms response', res);
     })
     res.send({success: true});
 }
